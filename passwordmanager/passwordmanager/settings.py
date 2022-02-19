29d4c90d752461588c82022edc3d17b1d100adee
cnkgkce <52961177+cnkgkce@users.noreply.github.com>
2022-02-19 23:25:55 +0300

    Update settings.py

--- passwordmanager/passwordmanager/settings.py
+++ passwordmanager/passwordmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r#+cc&4!kvf(oj+064^n6g+zdwlts$5rnro1v=4nv9+$4_xp7-'
+SECRET_KEY = '<redacted>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
